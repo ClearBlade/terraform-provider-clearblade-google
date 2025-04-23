e6e30c8df75477421dc31cd184ccc587decfe914
Sanket Deshpande <sanket@clearblade.com>
2025-04-23 09:01:16 -0700

    check if secret exists before create

--- internal/provider/secrets.go
+++ internal/provider/secrets.go
@@ -7,7 +7,19 @@ import (
 	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 )
 
+func secretExists(ctx context.Context, client *secretmanager.Client, projectId, secretId string) error {
+	resource := getSecretResourceName(projectId, secretId)
+	_, err := client.GetSecret(ctx, &secretmanagerpb.GetSecretRequest{Name: resource})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func createSecret(ctx context.Context, client *secretmanager.Client, projectId, secretId string) error {
+	if err := secretExists(ctx, client, projectId, secretId); err == nil {
+		return nil
+	}
 	parent := "projects/" + projectId
 	createReq := &secretmanagerpb.CreateSecretRequest{
 		Parent:   parent,
